de6cc0a7bd81d843e291db242e1fff13814c2cfe
Maksim V <maks112v@gmail.com>
2020-01-15 13:34:31 -0500

    in class lecture

---
+++ csc201/chapter1/src/lecture/FirstProgram.java
@@ -0,0 +1,21 @@
+package lecture;
+
+/**
+ * say "Hello World"
+ * 2020-01-15
+ * @author Maks Vakarchuk
+ */
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("hello, world");
+    }
+}
+
+// this is a comment
+// parts of a program:
+// keywords
+// identifiers
+// operators
+// punctuation
+// comments
+// syntax
