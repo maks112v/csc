2adc277b1df8b1189dddcecbfcb448d1b4480b71
Maksim V <maks112v@gmail.com>
2020-02-19 12:58:28 -0500

    added class of boat

---
+++ csc201/chapter1/src/Boat/Boat.java
@@ -0,0 +1,70 @@
+package Boat;
+
+/**
+ * Boat Class
+ * 2020/02/19
+ * @author Alex Hardy
+ * @author Maksim Vakarchuk
+ * @author Matt Nawton
+ */
+public class Boat {
+    private String name;
+    private String type;
+    private double length;
+
+    /**
+     * Returns name of current boat instance
+     * @return string of boat name
+     */
+    public String getName(){
+        return name;
+    }
+
+    /**
+     * Sets name of current boat instance
+     * @param boatName user input
+     */
+    public void setName(String boatName) {
+        name = boatName;
+    }
+
+    /**
+     * Returns type of current boat instance
+     * @return string of boat type
+     */
+    public String getType(){
+        return type;
+    }
+
+    /**
+     * Sets type of current boat instance
+     * @param boatType user input
+     */
+    public void setType(String boatType) {
+        type = boatType;
+    }
+
+    /**
+     * Returns length of current boat instance
+     * @return double of boat length
+     */
+    public double getLength() {
+        return length;
+    }
+
+    /**
+     * Sets length of current boat instance
+     * @param boatLength user input
+     */
+    public void setLength(double boatLength) {
+        length = boatLength;
+    }
+
+    /**
+     * Returns string of current boats name, type, and length
+     * @return string with info
+     */
+    public String getString() {
+        return "Boat{name=" + getName() +", type=" + getType() + "', length=" + getLength() + "}";
+    }
+}
---
+++ csc201/chapter1/src/Boat/BoatMain.java
@@ -0,0 +1,41 @@
+package Boat;
+import java.util.Scanner;
+
+/**
+ * Boat Init
+ * 2020/02/19
+ * @author Alex Hardy
+ * @author Maksim Vakarchuk
+ * @author Matt Nawton
+ */
+public class BoatMain {
+    private static Scanner scnr = new Scanner(System.in); // User Input Scanner
+
+    /**
+     * Starts running boat main
+     * @param args
+     */
+    public static void main(String[] args) {
+        Boat boat1 = new Boat(); // New Boat Instance
+        Boat boat2 = new Boat(); // New Boat Instance
+
+        System.out.print("Please enter the name of boat #1: ");
+        boat1.setName(scnr.nextLine()); // User input for boat 1 name
+        System.out.print("Please enter the type of boat #1: ");
+        boat1.setType(scnr.nextLine()); // User input for boat 1 type
+        System.out.print("Please enter the length of boat #1: ");
+        boat1.setLength(scnr.nextDouble()); // User input for boat 1 length
+
+        scnr.nextLine();
+        System.out.print("Please enter the name of boat #2: ");
+        boat2.setName(scnr.nextLine()); // User input for boat 2 name
+        System.out.print("Please enter the type of boat #2: ");
+        boat2.setType(scnr.nextLine()); // User input for boat 2 type
+        System.out.print("Please enter the length of boat #2: ");
+        boat2.setLength(scnr.nextDouble()); // User input for boat 2 length
+
+        System.out.println(boat1.getString());
+        System.out.println(boat2.getString());
+    }
+
+}
